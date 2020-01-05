db3ed12b6f5905638d179e6e5cdd4dcc43b6f459
Jatin dawar <jatindawar26@gmail.com>
2020-01-05 16:25:55 -0700

    Adding CBHE Approach

--- CBHE Approach/Histogram_Equalization_and_Color_Balancing.py
+++ CBHE Approach/Histogram_Equalization_and_Color_Balancing.py
@@ -21,10 +21,24 @@ from skimage.color import rgb2hsv,hsv2rgb
 
 class ImageEnhancement:
     def gaussian_blurring(self,input_image,kernel_size,sigma):
+        """Smoothening of an image.
+        @param input_img: The source image.
+        @kernel_size: size of the blur filter
+        @sigma: Standard deviation for Gaussian kernel.
+        @return The blurred image.
+        """
+        #Applying Gaussian Blur filter
         output_image=cv2.GaussianBlur(input_image,kernel_size,sigma)
         # output_image=cv2.medianBlur(input_image,kernel_size)
         return output_image
     def sampling(self,input_image,width,height):
+        """sampling of the image.
+        @param input_img: The image to be sampled.
+        @width: width to which the image needs to be sampled to.
+        @height: height to which the image needs to be sampled to.
+        @return The sampled image.
+        """
+        #Resizing the image
         output_image=cv2.resize(input_image,None,fx=width,fy=height)
         return output_image
     def get_dark_channel(self,img, *, size):
@@ -33,6 +47,7 @@ class ImageEnhancement:
         @param size: Patch size.
         @return The dark channel of the image.
         """
+        #Extract the dark/hazy part from the image
         minch = np.amin(img, axis=2)
         box = cv2.getStructuringElement(cv2.MORPH_RECT, (size // 2, size // 2))
         return cv2.erode(minch, box)
@@ -44,6 +59,7 @@ class ImageEnhancement:
         considered for the estimation.
         @return The estimated atmospheric light.
         """
+        #Get the atmospheric light factor from the image
         m, n, _ = img.shape
 
         flat_img = img.reshape(m * n, 3)
@@ -76,7 +92,9 @@ class ImageEnhancement:
         @param radius: (default: 40) Radius for the guided filter [2].
         @param epsilon: (default: 0.001) Epsilon for the guided filter [2].
         @return The final dehazed image.
+        
         """
+        #Applying atmosheric scattering model on the image
         L=356
         atmosphere = self.get_atmospheric_light(img, size=size, percent=percent)
         trans = self.get_transmission(img, atmosphere, size=size, omega=omega, radius=radius, epsilon=epsilon)
@@ -86,10 +104,16 @@ class ImageEnhancement:
 
 
     def process_imgdir(self,imgdir):
+        """Get haze free images in the directory
+        @param imgdir: The source image directory.
+        @return All the haze free images.
+        """
+          #Write images into resultdir
         resultdir = os.path.join(imgdir, 'results')
         inputdir = os.path.join(imgdir, 'inputs')
         shutil.rmtree(resultdir)
         os.mkdir(resultdir)
+        #Read images from input dir
         for fullname in os.listdir(inputdir):
             filepath = os.path.join(inputdir, fullname)
             if os.path.isfile(filepath):
@@ -105,13 +129,24 @@ class ImageEnhancement:
                 cv2.imwrite(os.path.join(resultdir, basename), dehazed)
         return os.path.join(resultdir, basename)
 
+        #Funtion for Color Balancing of the images
 
     def cal_equalisation(self,img,ratio):
+        '''
+        @param img: The source hazy image
+        @param ratio: RGB average ratio
+        @return the array after averaging the color
+        '''
         Array = img * ratio
         Array = np.clip(Array, 0, 255)
         return Array
 
+
     def RGB_equalisation(self, img):
+        '''
+        @param img: source hazy image
+        @return: Color balanced image
+        '''
         img = np.float32(img)
         avg_RGB = []
         for i in range(3):
@@ -121,25 +156,40 @@ class ImageEnhancement:
         a_r = avg_RGB[0]/avg_RGB[2]
         a_g =  avg_RGB[0]/avg_RGB[1]
         ratio = [0,a_g,a_r]
-        # print(img.shape)
+      
         for i in range(1,3):
             img[:,:,i] = self.cal_equalisation(img[:,:,i],ratio[i])
-            # print(img[:,:,i])
-        # print(img.shape)
+         
         return img
    
-    def image_enhancement(self,img):
+# Dehazing and enhancing the dehazed image
+    
+    def image_enhancement(self,img,file_name):
+        """Main function to call all the functions
+        @param img: Input image
+        @param file_name: The output file name
+        @return All the haze free images.
+        """
+        #Creating output directory if it doesnt exist
+        dirname = 'output'
+        dir_path = os.path.dirname(os.path.realpath(__file__))
+        if(os.path.isdir(os.path.join(dir_path, dirname))): 
+            if(os.path.exists(os.path.join(dir_path, dirname))):
+                pass
+        else:
+            os.mkdir(os.path.join(dir_path, dirname))
+            os.mkdir(os.path.join(dir_path, dirname,"results"))
+            os.mkdir(os.path.join(dir_path, dirname,"inputs"))
 
-    #Adding Histogram equalization and Color balacing
+        #Performing Color Balancing on the images
 
-        edges = cv2.Canny(img,80,255)
-        plt.imshow(edges)
-        plt.show()
+    
         kernel = (3,3)
 
         sceneRadiance = self.RGB_equalisation(img)
         sceneRadiance=sceneRadiance.astype(np.uint8)
-       
+        #Histogram Equalization of the color balanced image
+
         R, G, B = cv2.split(sceneRadiance)
         output1_R = cv2.equalizeHist(R)
         output1_G = cv2.equalizeHist(G)
@@ -147,106 +197,61 @@ class ImageEnhancement:
 
         equ = cv2.merge((output1_R, output1_G, output1_B))
 
-        
+        #Smoothening of the histogram equalized image using Gaussian Filter       
         gaussian_blurred_image =self.gaussian_blurring(equ,kernel,0)
         plt.subplot(121),plt.imshow(img),plt.title('Original')
         plt.xticks([]), plt.yticks([])
         plt.subplot(122),plt.imshow(gaussian_blurred_image),plt.title('Averaging')
         plt.xticks([]), plt.yticks([])
-        # plt.show()
+        #Down sizing of the smoothen image
+       
         coarse_image =self.sampling(gaussian_blurred_image,0.25,0.25)
-        dirname = 'test'
-        dir_path = os.path.dirname(os.path.realpath(__file__))
-        os.mkdir(os.path.join(dir_path, dirname))
-        os.mkdir(os.path.join(dir_path, dirname,"results"))
-        os.mkdir(os.path.join(dir_path, dirname,"inputs"))
+    
         
         cv2.imwrite(os.path.join(dir_path, dirname,'inputs','coarse_image.png'),coarse_image)
         plt.imshow(coarse_image)
         plt.title("Coarse Image")
-        # plt.show()
-        up_sampling=self.sampling(coarse_image,4,4)
-        gaus=self.gaussian_blurring(up_sampling,kernel,0)
-        # gaus=up_sampling
+
+        #Smoothening of the downsampled image to remove any artifacts
+        
+        
+        gaus=self.gaussian_blurring(coarse_image,kernel,0)
+        #Umsampling of the smoothened image
+
+        up_sampling=self.sampling(gaus,4,4)
+        
         plt.imshow(gaus)
-        # plt.show()
-        gaussian_blurred_image=cv2.resize(gaussian_blurred_image,(gaus.shape[1],gaus.shape[0]))
-        gaus_gray=cv2.cvtColor(gaus,cv2.COLOR_BGR2GRAY)
-        dst_gray=cv2.cvtColor(gaussian_blurred_image,cv2.COLOR_BGR2GRAY)
-        (score, diff) = compare_ssim(gaus_gray, dst_gray, full=True)
-        diff = (diff * 255).astype("uint8")
-        detail_image = cv2.subtract(gaus,gaussian_blurred_image)
-        plt.imshow(detail_image)
-        # plt.show()    
 
-
+        #Resizing of the image to 256*256 
+       
+        gaussian_blurred_image=cv2.resize(gaussian_blurred_image,(up_sampling.shape[1],up_sampling.shape[0]))
+        
+        #Resizing the file to compare it with other methods
+        resized_image = resize_img = cv2.resize(gaussian_blurred_image  , (256 , 256))
+          
         
         output_path=self.process_imgdir(os.path.join(dir_path, dirname))
         print(output_path)
         dehazed_image=cv2.imread(output_path)
-        plt.imshow(dehazed_image)
+        # plt.imshow(dehazed_image)
         # plt.show()    
         dehazed_image =self.sampling(dehazed_image,4,4)
         output_path="\\".join(output_path.split("\\")[:-1])
         cv2.imwrite(os.path.join(output_path,'dehazed_image.png'),dehazed_image)
         plt.imshow(dehazed_image)
 
+        #Writing the dehazed image into the result folder       
         
-        # R, G, B = cv2.split(img)
-
-        # output1_R = cv2.equalizeHist(R)
-        # output1_G = cv2.equalizeHist(G)
-        # output1_B = cv2.equalizeHist(B)
-
-        # equ = cv2.merge((output1_R, output1_G, output1_B))
+        cv2.imwrite(os.path.join(output_path,'finaldehazedimage.png'),resized_image )
         
-       
-    # clahe = cv2.createCLAHE(clipLimit=2, tileGridSize=(2, 2))
-        
-      
-
-        # plt.show()    
-        dst = cv2.addWeighted(detail_image,1,dehazed_image,1,0)
-        plt.imshow(dst)
-        # plt.show()    
-        kernel = np.array([[-1,-1,-1], [-1,9,-1], [-1,-1,-1]])
-        dst = cv2.filter2D(dst, -1, kernel)
-        
-        lab= cv2.cvtColor(dst, cv2.COLOR_BGR2LAB)       
-        l, a, b = cv2.split(lab)
-        clahe = cv2.createCLAHE(clipLimit=3.0, tileGridSize=(8,8))
-        cl = clahe.apply(l) 
-        limg = cv2.merge((cl,a,b))
-
-        final = cv2.cvtColor(limg, cv2.COLOR_LAB2BGR)
-        
-        cv2.imwrite(os.path.join(output_path,'final_output123.png'),gaussian_blurred_image )
-        psf = np.ones((5, 5)) / 25
-        dst=cv2.fastNlMeansDenoisingColored(gaussian_blurred_image,None,10,10,7,21)
-        
-        edges=cv2.cvtColor(edges,cv2.COLOR_GRAY2BGR)
-        print(edges.shape)
-        edges=cv2.resize(edges,(dst.shape[1],dst.shape[0]))
-        print(dst.shape)
-        dst = cv2.addWeighted(dst,1,edges,1,0)
-        hsv = cv2.cvtColor(dst, cv2.COLOR_BGR2HSV)
-        h,s,v=cv2.split(hsv)
-        value = 30 #whatever value you want to add
-        lim=255-value
-        
-        s[s>lim]=255
-        s[s<lim]+=value
-        value1=30
-        lim1=255-value1
-        v[v>lim1]=255
-        v[v<lim1]+=value1
-        hsv[:,:,2] += value1
-        hsv[:,2,:] += value
-        hsv = cv2.merge((h, s, v))
-        dst = cv2.cvtColor(hsv, cv2.COLOR_HSV2BGR)
-        
-        cv2.imwrite(os.path.join(output_path,'final_output.png'),dst)
 
 if __name__=="__main__":
-    img = cv2.imread("C:\\Users\\jatin\Desktop\\803 Project\\Testing_image\\0154_0.8_0.2.jpg")
-    ImageEnhancement().image_enhancement(img)
+     #Input file path
+    path = "dataset\\0042_0.8_0.2.jpg"
+    #loading the file
+    img = cv2.imread(path)
+    print(img)
+    #Extracting file name
+    file_name=".".join(path.split("\\")[-1].split(".")[:-1])
+    #calling the function
+    ImageEnhancement().image_enhancement(img,file_name)
---
+++ CBHE Approach/readME.txt
@@ -0,0 +1,12 @@
+1. Install Python from:-
+        https://www.python.org/ftp/python/3.7.6/python-3.7.6-amd64.exe
+ 
+2. Open command prompt and go to the folder location "CBHE Approach"
+        cd "CBHE Approach"
+3. Put the test images in folder dataset or use any image in the dataset folder
+
+4. Type:
+        python "Task-Specific-Image-Enhancement/Histogram_Equalization_and_Color_Balancing.py"
+
+5. Output will be saved in Output/results
+
